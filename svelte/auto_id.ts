1212690a60e296d448e6e0883a080e9884f8da62
Daniel Imfeld <daniel@imfeld.dev>
2023-11-28 14:57:17 -1000

    replace Labelled with SideLabelled and TopLabelled

---
+++ svelte/auto_id.ts
@@ -0,0 +1,10 @@
+const PREFIX = 'app';
+
+let counter = 0;
+export function newId(name?: string) {
+  if (name) {
+    return `${PREFIX}-${name}-${++counter}`;
+  } else {
+    return `${PREFIX}-${++counter}`;
+  }
+}
